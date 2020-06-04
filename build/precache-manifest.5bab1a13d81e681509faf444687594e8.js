382c4985e969ece06d8180641ec2a9cc482c224a
Alexander P <o.pyliavets@gmail.com>
2020-06-04 20:12:55 +0300

    add build

---
+++ build/precache-manifest.5bab1a13d81e681509faf444687594e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd078da619c057e819ab0c35fe9b5ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "531299b7ab72f96b5137",
+    "url": "/static/css/main.34fa6e8e.chunk.css"
+  },
+  {
+    "revision": "32540ac14b0f9e67caa5",
+    "url": "/static/js/2.ac17b5d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ac17b5d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "531299b7ab72f96b5137",
+    "url": "/static/js/main.fe3fd857.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
